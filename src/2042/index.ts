7be457c18322e8808f10a2c7906aa2ed28b1ac3f
Innam Park <pin954562@gmail.com>
2023-06-04 22:00:39 +0900

    fix

--- src/2042/index.ts
+++ src/2042/index.ts
@@ -3,9 +3,12 @@ import {readFileSync} from 'fs';
 const inputPath = 'src/2042/input.txt';
 // const inputPath = '/dev/stdin';
 const inputList = readFileSync(inputPath).toString().split('\n');
-const [N] = inputList[0].split(' ').map(Number);
+const N = Number(inputList[0].split(' ')[0]);
 
-function sum(tree: bigint[], i: number) {
+const temp: bigint[] = Array.from({length: N + 1}, () => BigInt(0));
+const tree: bigint[] = Array.from({length: N + 1}, () => BigInt(0));
+
+function sum(i: number) {
   let ret = BigInt(0);
   while (i > 0) {
     ret += tree[i];
@@ -14,18 +17,15 @@ function sum(tree: bigint[], i: number) {
   return ret;
 }
 
-function update(tree: bigint[], i: number, dif: bigint) {
+function update(i: number, dif: bigint) {
   while (i < tree.length) {
     tree[i] += dif;
     i += i & -i;
   }
 }
 
-const numList: bigint[] = Array.from({length: N + 1}, () => BigInt(0));
-const tree: bigint[] = Array.from({length: N + 1}, () => BigInt(0));
 for (let i = 1; i <= N; i++) {
-  numList[i] = BigInt(inputList[i]);
-  update(tree, i, numList[i]);
+  update(i, (temp[i] = BigInt(inputList[i])));
 }
 
 for (let i = N + 1; i < inputList.length; i++) {
@@ -35,11 +35,12 @@ for (let i = N + 1; i < inputList.length; i++) {
   if (a === 1) {
     const b = Number(cmd[1]);
     const c = BigInt(cmd[2]);
-    const dif = c - numList[b];
-    update(tree, b, dif);
+    const dif = c - temp[b];
+    temp[b] = c;
+    update(b, dif);
   } else {
     const b = Number(cmd[1]);
     const c = Number(cmd[2]);
-    console.log((sum(tree, c) - sum(tree, b - 1)).toString());
+    console.log((sum(c) - sum(b - 1)).toString());
   }
 }
